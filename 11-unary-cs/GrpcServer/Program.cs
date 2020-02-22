dadf239e47318b98dc2b2cc053af1aa8d35b3305
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2020-02-22 16:54:24 +0530

    new proj based on boilerplate

---
+++ 11-unary-cs/GrpcServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using System;
+using System.IO;
+
+namespace GrpcServer
+{
+    class Program
+    {
+        const int port = 50051;
+        static void Main(string[] args)
+        {
+
+            Server server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", port, SslServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine($"Server running at port {port}");
+                Console.ReadKey();    
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Failed to start server for {ex.Message}");
+                throw;
+            }
+            finally
+            {
+                if(server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
+        }
+    }
+}
