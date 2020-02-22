eda3b5c5a1fdc30de5b3111d95e4f67102263d3b
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2020-02-22 16:51:29 +0530

    added a csharp biolerplate project

---
+++ 10-grpc-boilerplate-cs/GrpcClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Dummy;
+using Grpc.Core;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        const String target = "127.0.0.1:50051";
+        static void Main(string[] args)
+        {
+            Channel channel = null;
+
+            try
+            {
+                channel = new Channel(target, ChannelCredentials.Insecure);
+                channel.ConnectAsync().ContinueWith(task =>
+                {
+                    if (task.Status == System.Threading.Tasks.TaskStatus.RanToCompletion)
+                    {
+                        Console.WriteLine("Client Connection Success!!!");
+                    }
+                });
+
+                var client = new DummyService.DummyServiceClient(channel);
+                channel.ShutdownAsync().Wait();
+                Console.ReadKey();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed for reason",ex.Message);
+                throw;
+            }
+        }
+    }
+}
